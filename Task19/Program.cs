3950e171dfef99cfae5a3a88c45010343ba9c2fd
Borisenko Natalli <nata251284@gmail.com>
2023-02-02 22:20:03 +0300

    Прога проверяет является ли пятизначное число палиндромом

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+
+*/ 
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string ThreeDigit = Convert.ToString(number);
+
+if (ThreeDigit[0]==ThreeDigit[4] && ThreeDigit[1]==ThreeDigit[3])
+Console.WriteLine ("Число является палиндромом");
+
+else Console.WriteLine ("Число не является палиндромом");
